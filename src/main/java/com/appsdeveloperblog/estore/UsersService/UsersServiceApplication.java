9493abf921101f0ac705320214546a0edeebf9dd
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2023-05-02 13:47:43 -0400

    Fixed issue with XStream ForbiddenClassException

--- src/main/java/com/appsdeveloperblog/estore/UsersService/UsersServiceApplication.java
+++ src/main/java/com/appsdeveloperblog/estore/UsersService/UsersServiceApplication.java
@@ -2,7 +2,11 @@ package com.appsdeveloperblog.estore.UsersService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+import com.appsdeveloperblog.estore.core.config.XStreamConfig;
+
+@Import({ XStreamConfig.class })
 @SpringBootApplication
 public class UsersServiceApplication {
 
